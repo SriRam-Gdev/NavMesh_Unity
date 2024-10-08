12c99511cb32780a2ef10ba3ad19b5db093eb3fe
Sriram s <123556950+SriRam-Gdev@users.noreply.github.com>
2024-10-08 13:48:54 +0530

    l

---
+++ Assets/playercontrol.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class playercontrol : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
